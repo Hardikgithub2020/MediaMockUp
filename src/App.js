7d01bdf14dda675c55772e86bd0c2026239ab30e
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-21 16:58:57 -0400

    Second Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header'
+import Section1 from './Components/Section1'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Section1/>
+    </div>
+  );
+}
+
+export default App;
